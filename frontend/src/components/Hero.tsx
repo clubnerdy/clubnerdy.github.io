c0793b8a35323a86a929049ddcb9b3c315cab8cd
hoejeong <seohoejeong@gmail.com>
2025-08-27 17:55:56 +0900

    진횅중

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import "../styles/hero.css";
 import React from "react";
 import Audio from "./Audio";
 
@@ -6,7 +7,19 @@ function Hero() {
     <>
       <Audio />
       <div className="container">
-        <h1>메인비주얼</h1>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+        <div className="hero-title">
+          <h1>We're</h1>
+          <h1>Getting</h1>
+          <h1>Married</h1>
+        </div>
+        <div className="hero-sub-title">
+          <p>2025년 11 월 1일 (토) 12 : 30</p>
+          <p>센텀 사이언스파크</p>
+          <p>별관 4층 더하우스홀</p>
+        </div>
       </div>
     </>
   );
